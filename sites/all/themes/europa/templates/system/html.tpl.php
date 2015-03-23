afb6682bee8a52fcde827adc414dae84a7c8e8cd
Janos Kuszing <github.com@office.netstudio.hu>
2015-03-23 19:36:50 +0100

    NEXTEUROPA-2231: Intermediate commmit.
    Former-commit-id: bc407a37c8a65742730c85ddcb1db8c64cc9b9a9

--- sites/all/themes/europa/templates/system/html.tpl.php
+++ sites/all/themes/europa/templates/system/html.tpl.php
@@ -60,8 +60,8 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  <div id="skip-link" class="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable" tabindex="1"><?php print t('Skip to main content'); ?></a>
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
