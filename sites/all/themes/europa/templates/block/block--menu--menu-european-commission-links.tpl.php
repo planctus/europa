81d303b13e5b3d30afb278f1774660a0e9c282b5
Janos Kuszing <github.com@office.netstudio.hu>
2015-03-26 16:04:31 +0100

    NEXTEUROPA-1891: Modified block--menu--menu-european-commission-links.tpl.php.
    Former-commit-id: 3c1ede12a1f932de9f3b412d6283d5f8c3636ebf

--- sites/all/themes/europa/templates/block/block--menu--menu-european-commission-links.tpl.php
+++ sites/all/themes/europa/templates/block/block--menu--menu-european-commission-links.tpl.php
@@ -47,12 +47,12 @@
 ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-        <label<?php print $title_attributes; ?>><?php print $title; ?></label>
-    <?php endif;?>
-    <?php print render($title_suffix); ?>
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <label<?php print $title_attributes; ?>><?php print $title; ?></label>
+  <?php endif;?>
+  <?php print render($title_suffix); ?>
 
-    <?php print $content ?>
+  <?php print $content; ?>
 
 </section> <!-- /.block -->
