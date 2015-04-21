3dc7604a7d17f21b86f4d8ed64a11a8320b49945
Kris Gunciarz <kris@waat.eu>
2015-04-21 15:33:47 +0200

    NEXTEUROPA-3182 adding new layout for NE DS, adding this new layout to dt_announcement
    Former-commit-id: 98bd4cb53a631fa80128ffeebacbc35fb5f8a239

---
+++ project/modules/custom/nexteuropa_ds_layouts/layouts/ne_2col_stacked/ne-2col-stacked.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Display Suite NE Bootstrap Two Columns Stacked.
+ */
+
+  // Add sidebar classes so that we can apply the correct width in css.
+?>
+
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes; ?>">
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  <?php if (!empty($second)): ?>
+    <<?php print $second_wrapper; ?> class="column-second <?php print $second_classes; ?>">
+      <?php print $second; ?>
+    </<?php print $second_wrapper; ?>>
+  <?php endif; ?>
+  <<?php print $main_wrapper; ?> class="column-main <?php print $main_classes; ?>">
+    <div class="meta">
+      <?php print $meta; ?>
+    <div>
+    <?php print $main; ?>
+  </<?php print $main_wrapper; ?>>
+  </div>
+</<?php print $layout_wrapper ?>>
+
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
