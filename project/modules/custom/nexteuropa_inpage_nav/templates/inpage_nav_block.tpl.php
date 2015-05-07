6a71b74436e1fc53adc39bf4321e412ca3452ed6
Kugler Tamas <tamas.kugler@gmail.com>
2015-05-07 09:34:47 +0200

    NEXTEUROPA-1893
    Former-commit-id: 0a5ae188a931c420d711a691a889f9f0f7ed8eb4

--- project/modules/custom/nexteuropa_inpage_nav/templates/inpage_nav_block.tpl.php
+++ project/modules/custom/nexteuropa_inpage_nav/templates/inpage_nav_block.tpl.php
@@ -1,16 +1,24 @@
+<?php
+/**
+ * @file
+ * Template for in-page navigation.
+ */
+?>
 <div class="inpage-nav">
-    <nav class="navbar navbar-default--blue navbar-fixed-top">
+    <nav class="navbar navbar-default navbar-fixed-top inpage-nav__navbar">
         <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".inpage-navigation--list" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only"><?php echo t("Toggle navigation"); ?></span>
+            <div class="navbar-header"  data-toggle="collapse" data-target="#inpage-navigation-list" aria-expanded="false" aria-controls="navbar">
+                <button type="button" class="navbar-toggle collapsed">
+                    <span class="sr-only"><?php print t("Toggle navigation"); ?></span>
                     <span class="arrow-down"></span>
-                    <span class="close-text"><?php echo t("Close");?></span>
+                    <span class="close-text"><?php print t("Close");?></span>
                 </button>
-                <p class="navigation-place"><?php echo (isset($node_title) ? $node_title : ''); ?></p>
+                <span class="inpage-nav__help"><?php print t('On this page'); ?></span>
+                <span class="navbar-brand inpage-nav__current"><?php print $title; ?></span>
             </div>
-            <div class="inpage-nav__list navbar-collapse collapse">
-                <?php echo theme('item_list', $vars); ?>
+            <div class="navbar-collapse collapse inpage-nav__list" id="inpage-navigation-list">
+              <span class="inpage-nav__title" ><?php print $title; ?></span>
+                <?php print $links; ?>
             </div>
         </div>
     </nav>
