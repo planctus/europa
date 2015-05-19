f15abe57349ef466bc2b08991062377286dcad08
Kalin Chernev <kalin.chernev@gmail.com>
2015-05-19 14:19:18 +0200

    NEXTEUROPA-1948: Fixes
    Former-commit-id: 3e5c80663d047ba67d5f48277f67bab1f572fcd0

--- project/themes/europa/templates/views/views-view-unformatted.tpl.php
+++ project/themes/europa/templates/views/views-view-unformatted.tpl.php
@@ -10,11 +10,6 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<?php if ($active_filters): ?>
-  <div class="view-header">
-    <?php print $active_filters; ?>
-  </div>
-<?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
   <div class="<?php print $classes_array[$id] . ' ' . $additional_classes_array; ?>">
     <?php print $row; ?>
