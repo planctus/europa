df83472e16ec6c190b8331027e1900cc20f65c21
Kugler Tamas <tamas.kugler@gmail.com>
2015-06-01 09:47:52 +0200

    NEXTEUROPA-1893 fixes after review
    Former-commit-id: 56ded9d3ebcd73e55dc736c0b4f82cc1dc479bed

--- project/modules/custom/nexteuropa_inpage_nav/js/inpage_nav.js
+++ project/modules/custom/nexteuropa_inpage_nav/js/inpage_nav.js
@@ -1,11 +1,9 @@
 (function ($) {
   Drupal.behaviors.inpage_navigation = {
     fixWidth: function($e, $parent) {
-      // Adjust width dinamcally.
+      // Adjust width dinamcally to parent.
       var blockWidth = 'auto';
-      if ($e.hasClass('is-affixed')) {
-        blockWidth = $parent.width() + 'px';
-      }
+      blockWidth = $parent.width() + 'px';
       $e.css({
         'width' : blockWidth
       });
@@ -34,34 +32,72 @@
         enquire.register("screen and (min-width: 1200px)", {
           // desktop
           match : function() {
-            $inPageActive = true;
-            // Page smooth scrolling on anchor click
-            $('a[href^="#"]', this).click(function(e) {
-              e.preventDefault();
-
-              sectionOffset = $(this.hash).offset().top;
-
-              if ($(this).parent().is(':first-child')) {
-                sectionOffset = sectionOffset - 20;
+            // Affix.
+            Drupal.behaviors.inpage_navigation.fixWidth($inPageBlock, $inPageBlockParent);
+            $inPageBlock.affix({
+              offset: {
+                top: function () {
+                  return(Math.floor($inPageBlock.parent().offset().top - 15));
+                },
+                bottom: function () {
+                  return($('footer.footer').outerHeight() + $('footer.footer-top').outerHeight() + 20);
+                }
               }
+            });
 
-              $('html, body').velocity("scroll", {easing:'ease', duration: 400, offset: sectionOffset});
+            $inPageBlock.on('affix.bs.affix', function() {
+              $inPageBlock.addClass('is-fixed');
+            });
+
+            $inPageBlock.on('affixed.bs.affix', function() {
+              $inPageBlock.addClass('is-fixed');
+              $inPageBlock.removeClass('is-bottom');
+            });
+
+            $inPageBlock.on('affixed-top.bs.affix', function() {
+                $inPageBlock.addClass('is-fixed');
+            });
+
+            $inPageBlock.on('affixed-bottom.bs.affix', function() {
+                $inPageBlock.addClass('is-bottom');
             });
           },
 
           setup: function() {
             var $navBar = $('<div class="inpage-nav inpage-nav__navbar-wrapper"><nav class="navbar navbar-default navbar-fixed-top inpage-nav__navbar"><div class="container inpage-nav__container"><div class="navbar-header inpage-nav__header"  data-toggle="collapse" data-target="#inpage-navigation-list" aria-expanded="false" aria-controls="navbar"><button type="button" class="navbar-toggle collapsed inpage-nav__toggle"><span class="sr-only">' + Drupal.t("Toggle navigation") + '</span><span class="inpage-nav__icon-arrow icon icon--arrow-down"></span></button><span class="navbar-brand inpage-nav__help">' +  Drupal.t('On this page') + '</span><div class="inpage-nav__current-wrapper"><span class="navbar-brand inpage-nav__current">' +  title + '</span></div></div><div class="navbar-collapse collapse inpage-nav__list" id="inpage-navigation-list"><span class="inpage-nav__title" >' +  title + '</span>' + $inPage.html() + '</div></div></nav>');
             var $navBarHeader = $('.inpage-nav__header', $navBar);
-            var $navBarList = $('.inpage-nav__list', $navBar);
+            var $navBarList = $('#inpage-navigation-list', $navBar);
             var $navBarCurrent = $('.inpage-nav__current', $navBar);
             var $navBarTitle = $('.inpage-nav__title', $navBar);
             var $navBarHelp = $('.inpage-nav__help', $navBar);
 
+            // Page smooth scrolling on anchor click from sidebar.
+            $('a[href^="#"]', $inPage).click(function(e) {
+              e.preventDefault();
+              var sectionOffset = $(this.hash).offset().top;
+              $('html, body').velocity("scroll", {easing:'ease', duration: 400, offset: sectionOffset});
+            });
+
+            // Page smooth scrolling on anchor click from navbar.
+            $('li a[href^="#"]', $navBar).click(function(e) {
+              e.preventDefault();
+              var sectionOffset = $(this.hash).offset().top - 50;
+              $('html, body').velocity("scroll", {easing:'ease', duration: 400, offset: sectionOffset});
+              $navBarList.collapse('hide');
+            });
+
+            // Hide if clicked outside.
+            $('.inpage-nav__navbar', $navBar).click(function(e) {
+              $navBarList.collapse('hide');
+            });
+
             $('body').append($navBar);
-            // Page navigation scroll spy
+            // Page navigation scroll spy.
             $('body').scrollspy({
                 target: '.inpage-nav'
             });
+
+
 
             $navBar.on('show.bs.collapse', function() {
               $navBar.addClass('is-collapsing');
@@ -81,6 +117,7 @@
               $navBarCurrent.text($("li.active > a", $navBar).text());
             });
 
+
             $(window).scroll(function(e) {
               $window = $(this);
               // Clear title for In page nav navbar title if nothing selected.
@@ -92,38 +129,22 @@
               // Show navbar if scroll is under the block.
               var docViewTop = $window.scrollTop();
               var docViewBottom = docViewTop + $window.height();
-              var inPageBottom = inPageBlockTop + inPageBlockHeight;
+              var inPageBottom = inPageBlockTop + inPageBlockHeight - 35;
               if (inPageBottom <= docViewTop) {
                 $navBar.addClass('is-active');
               } else {
                 $navBar.removeClass('is-active');
-              }
-
-              // Sidebar Inpage Nav.
-              if ($inPageActive) {
-                var safeHeight = inPageBlockHeight + $('#footer').outerHeight() + $('#footer-top').outerHeight() + 50;
-                var navOffset = $inPageBlock.parent().offset().top,
-                  scrollTopValue = $(window).scrollTop();
-                var atBottom = (scrollTopValue + safeHeight) > $('body').height();
-                if(!atBottom) {
-                  if (navOffset < scrollTopValue && !($inPageBlock.hasClass('affix'))) {
-                    $inPageBlock.addClass('affix is-affixed').removeClass('affix-top').removeClass('affix-bottom');
-                  }
-                  else if (navOffset > scrollTopValue && !($inPageBlock.hasClass('affix-top'))) {
-                    $inPageBlock.removeClass('affix is-affixed').addClass('affix-top');
-                  }
-                } else {
-                  $inPageBlock.removeClass('affix is-affixed').addClass('affix-bottom');
-                }
-                Drupal.behaviors.inpage_navigation.fixWidth($inPageBlock, $inPageBlockParent);
               }
             });
           },
 
           // mobile
           unmatch : function() {
-            $inPageActive = false;
-            $inPageBlock.removeClass('affix is-affixed affix-top affix-bottom');
+            // Swicth off affix.
+            $(window).off('.affix');
+            $inPageBlock
+              .removeClass("affix affix-top affix-bottom is-fixed is-bottom")
+              .removeData("bs.affix");
           }
         });
       });
