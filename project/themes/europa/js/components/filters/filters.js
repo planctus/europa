24f489b5b1d85e334320b70e0b0b2d674f068a26
Kris Gunciarz <kris@waat.eu>
2015-06-18 16:01:42 +0200

    NEXTEUROPA-1948 fixing combined filter behavior when no result, fixing breakpoints on filters
    Former-commit-id: 6b6b4a53e838f4233a0847d280425c54937b870d

--- project/themes/europa/js/components/filters/filters.js
+++ project/themes/europa/js/components/filters/filters.js
@@ -29,9 +29,9 @@
         $resultsCount
           .append(
             '<div class="btn-group">' +
-              '<button class="btn btn-default filters__btn-reset--small hidden-sm hidden-md hidden-lg">' + clearAll +
+              '<button class="btn btn-default filters__btn-reset--small">' + clearAll +
               '</button>' +
-              '<button class="btn btn-primary hidden-sm hidden-md hidden-lg filters__btn-collapse" type="button"' +
+              '<button class="btn btn-primary filters__btn-collapse" type="button"' +
               ' data-toggle="collapse" data-target="#' + Drupal.settings.europa.exposedBlockId + '"' +
               ' aria-expanded="false" aria-controls="collapseFilters">' +
                 refineText +
@@ -60,7 +60,7 @@
 
         if (typeof enquire !== 'undefined') {
           // Runs on device width change.
-          enquire.register('screen and (min-width: 768px)', {
+          enquire.register('screen and (min-width: 992px)', {
             // desktop
             match : function() {
               var $sidebarFirst = $(".region-sidebar-first"),
@@ -88,7 +88,7 @@
               var $sidebarFirst = $(".region-sidebar-first");
 
               // Showing buttons on viewport switch
-              showFilterButtons();
+              //showFilterButtons();
 
               $filters.wrapInner("<div class='filters__wrapper'></div>");
               $filters
