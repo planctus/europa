188e48ee79b759f729aeb8d0318433d9b82f3b76
Kugler Tamas <tamas.kugler@gmail.com>
2015-07-02 10:35:06 +0200

    NEXTEUROPA-3790 disabling search button
    Former-commit-id: b45467218632e564a1488cd516c485bab4b205aa

--- project/themes/europa/template.php
+++ project/themes/europa/template.php
@@ -861,7 +861,7 @@ function europa_form_nexteuropa_europa_search_search_form_alter(&$form, &$form_s
   $form['search_input_group']['europa_search_submit']['#attributes']['class'][] = 'search-form__btn';
   $form['search_input_group']['QueryText']['#attributes']['class'][] = 'search-form__textfield';
   // Popover to notify the user that the search is not fully functional.
-  $form['search_input_group']['QueryText']['#disabled'] = TRUE;
+  $form['search_input_group']['europa_search_submit']['#disabled'] = TRUE;
   $form['search_input_group']['QueryText']['#attributes']['data-toggle'][] = 'popover';
   $form['search_input_group']['QueryText']['#attributes']['data-placement'][] = 'bottom';
   $form['search_input_group']['QueryText']['#attributes']['data-trigger'][] = 'focus';
