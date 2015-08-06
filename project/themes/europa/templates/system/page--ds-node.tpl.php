49d67262f20ffd27c8e1cafec0b3a753e350341b
Kris Gunciarz <kris@waat.eu>
2015-08-07 00:02:15 +0200

    NEXTEUROPA-4785 css improvements for site header
    Former-commit-id: b23f96da7d7a3a3e1708569bdfd1e253d5e0cc1c

--- project/themes/europa/templates/system/page--ds-node.tpl.php
+++ project/themes/europa/templates/system/page--ds-node.tpl.php
@@ -111,11 +111,15 @@
 
     <?php if (!empty($page['header'])): ?>
       <section class="top-bar" aria-label="Site tools">
-        <?php if($is_front): ?>
-          <h1 class="sr-only"><?php print $site_name; ?></h1>
-          <h2 class="sr-only"><?php print t('Classes'); ?></h2>
-        <?php endif; ?>
-        <?php print render($page['header']); ?>
+        <div>
+          <div class="top-bar__wrapper">
+            <?php if($is_front): ?>
+              <h1 class="sr-only"><?php print $site_name; ?></h1>
+              <h2 class="sr-only"><?php print t('Classes'); ?></h2>
+            <?php endif; ?>
+            <?php print render($page['header']); ?>
+          </div>
+        </div>
       </section>
     <?php endif; ?>
   </div>
