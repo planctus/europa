269df0d36d0378af0b87b7ba67cb161b30b10b12
Kalin Chernev <kalin.chernev@gmail.com>
2015-09-03 14:30:42 +0200

    NEXTEUROPA-2237: Adding click listener in the select
    Former-commit-id: 4bbb38ab16729de6c7a39b484249b1e8fa7ec9c3

--- project/themes/europa/js/components/lang-switcher.js
+++ project/themes/europa/js/components/lang-switcher.js
@@ -5,8 +5,7 @@
         listClass = $list.attr('class');
 
     $list.each(function() {
-      var $select = $('<select />');
-      $select.addClass(listClass);
+      var $select = $('<select />').addClass(listClass);
 
       $(this).find('a').each(function() {
         var $option = $('<option />');
@@ -16,6 +15,16 @@
 
       if (!$list.parent().find('select').length) {
         $list.parent().append($select);
+        $select.on({
+          change: function(event) {
+            var optionHref = $(this).val(),
+                $item = $list.find('li'),
+                $location = $item.children('a[href="' + optionHref + '"]');
+
+            // $location.click(); // ajax-friendly
+            window.location.href = $location.attr('href');
+          }
+        });
       }
     });
     $list.hide();
