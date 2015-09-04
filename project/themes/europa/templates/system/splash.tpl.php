344f864874eece2924f3307ab7c2ce1561f849ac
Harings Rob <haringsrob@gmail.com>
2015-09-04 16:49:59 +0200

    NEXTEUROPA-2236 implement help text
    Former-commit-id: bdafcc66414d26ee6a1cb8482cb88e79cd8cdcfe

--- project/themes/europa/templates/system/splash.tpl.php
+++ project/themes/europa/templates/system/splash.tpl.php
@@ -9,6 +9,7 @@
  * - $languages_list: the language list.
  * - $languages_list_array: unformated array of languages.
  * - $languages_blacklist: unformated array of blacklisted language codes.
+ * - $splash_helper_text: Formatted html helper text.
  *
  * @see template_preprocess()
  * @see template_preprocess_splash()
@@ -19,5 +20,15 @@
   <h2><span class="icon icon--language"></span> <?php print $header_text; ?></h2>
 
   <?php print $languages_list; ?>
-  <?php print $close_button; ?>
+
+  <?php if (isset($splash_helper_text)): ?>
+    <div class="splash-page__helper-text">
+      <?php print $splash_helper_text; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($close_button)): ?>
+    <?php print $close_button; ?>
+  <?php endif; ?>
+
 </nav>
