1d338fd7458c2da8ef62a888d390f7bc7ade1978
planctus <davide@nois3lab.it>
2015-09-10 16:14:44 +0200

    NEXTEUROPA-5333 - Simplifying code more than applying a particular solution
    Former-commit-id: 4f1de68a4ba21967406dfa4758875140c0cb5785

--- project/themes/europa/styleguide/assets/public/js/europa.js
+++ project/themes/europa/styleguide/assets/public/js/europa.js
@@ -10,6 +10,11 @@
 
   Drupal.behaviors.europa_tabs = {
     attach: function (context) {
+      $('a[href^="http"]').each(function() {
+        var trimmed = $.trim($(this).text());
+        $(this).text(trimmed);
+      });
+
       $('.nav-tabs--with-content').once('nav-tabs', function() {
         $this = $(this);
         if (typeof enquire !== 'undefined') {
