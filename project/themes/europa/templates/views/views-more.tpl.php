e28283be80c7403fcd34495d02623fa7df7c410e
Harings Rob <haringsrob@gmail.com>
2015-10-01 16:41:25 +0200

    NEXTEUROPA-5258 changed implementation logic
    Former-commit-id: 714106c19be2539069008b6143be0de9d8626840

--- project/themes/europa/templates/views/views-more.tpl.php
+++ project/themes/europa/templates/views/views-more.tpl.php
@@ -11,7 +11,13 @@
  * @ingroup views_templates
  */
 ?>
-
+<?php
+// Sometimes we have a special url. In these cases we manually replace it.
+// We strictly limit it, because this is not replacing substitutions.
+if ($view->name == 'announcements' && $view->current_display == 'latest_block' && $view->display[$view->current_display]->handler->options['link_display'] == 'custom_url'):
+  $more_url = $view->display[$view->current_display]->handler->options['link_url'];
+endif;
+?>
 <div class="more-link">
   <a class="listing__read-more" href="<?php print $more_url ?>">
     <?php print $link_text; ?>
