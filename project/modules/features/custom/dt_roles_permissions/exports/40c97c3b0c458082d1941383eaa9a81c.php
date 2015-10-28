9e8bb00eaa60af08ba58923e09032fc541cab34c
Harings Rob <haringsrob@gmail.com>
2015-10-28 12:05:43 +0100

    NEXTEUROPA-6289 acceptance permission export
    Former-commit-id: 739b1c4eb7fcb2f8bdf2f48610d17bc14d4c10d4

---
+++ project/modules/features/custom/dt_roles_permissions/exports/40c97c3b0c458082d1941383eaa9a81c.php
@@ -0,0 +1,16 @@
+<?php
+$permissions['anonymous user'] = array(
+  'access comments',
+  'access content',
+  'access PDF version',
+  'access print',
+  'access site-wide contact form',
+  'access user profiles',
+  'post comments',
+  'search content',
+  'show format selection for file',
+  'use advanced search',
+  'use text format basic_html',
+  'use text format filtered_html',
+  'view files',
+);
---
+++ project/modules/features/custom/dt_roles_permissions/exports/912928679cd85e1c6b6c9f6c47836418.php
@@ -0,0 +1,12 @@
+<?php
+$permissions['site manager'] = array(
+  'access bean overview',
+  'access helper content types',
+  'administer permissions',
+  'administer users',
+  'bypass node access',
+  'bypass workbench moderation',
+  'configure helper content types',
+  'show format selection for file',
+  'view bean page',
+);
