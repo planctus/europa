52beb82961f012a1fb54801422319e355ebad8f9
planctus <davide@nois3lab.it>
2015-11-06 11:55:58 +0100

    NEXTEUROPA-6223 - reverting changes to the js, no modifications has been done on it
    Former-commit-id: 156c6dad71fef604e271cd3ed190cf46761fc7b2

--- project/themes/europa/js/components/breadcrumb.js
+++ project/themes/europa/js/components/breadcrumb.js
@@ -2,6 +2,7 @@
   Drupal.behaviors.europa_breadcrumb = {
     attach: function (context) {
       $('#breadcrumb').once('breadcrumb', function(){
+
         // Add collapsible class for js mobile behavior.
         var $breadcrumbWrapper = $('#breadcrumb');
         $breadcrumbWrapper.addClass('breadcrumb--collapsible');
