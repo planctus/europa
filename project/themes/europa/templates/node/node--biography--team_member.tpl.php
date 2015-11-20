cf6d09e4b9082b41f83905775ce57300e94ec8d4
planctus <davide@nois3lab.it>
2015-11-20 11:24:18 +0100

    improvements
    Former-commit-id: 7f209c3777c75051e025a437955cd4b99448d750

--- project/themes/europa/templates/node/node--biography--team_member.tpl.php
+++ project/themes/europa/templates/node/node--biography--team_member.tpl.php
@@ -3,6 +3,7 @@
  * @file
  * Define the display of a custom view mode.
  */
+
   // Those are required fields.
   $firstname = render($content['field_biography_first_name'][0]['#markup']);
   $lastname =  render($content['field_biography_last_name'][0]['#markup']);
@@ -10,11 +11,7 @@
   // Social networks, find the values to be able to print them.
   if (isset($content['field_social_networks'])) {
     $keys = array_keys($content['field_social_networks']);
-    $social = array_filter($keys,
-      function($n) {
-        return is_int($n);
-      }
-    );
+    $social = array_filter($keys, function($n) { return is_int($n); });
   }
 
   // Phone field, we format it as a link.
@@ -22,9 +19,10 @@
     $phone = render($content['field_biography_phone'][0]['#markup']);
   }
 ?>
-<div class="team-member">
-  <div class="team-member-top-wrapper">
-    <div class="team-member__picture field">
+
+<div class="listing--cabinet">
+  <div class="listing--cabinet-top-wrapper">
+    <div class="listing--cabinet__picture field">
       <?php print render($content['field_biography_portrait']); ?>
     </div>
 
@@ -34,12 +32,12 @@
     </span>
   <?php endif; ?>
 
-    <h4 class="team-member__name field">
+    <h4 class="listing--cabinet__name field">
       <?php print l($firstname . ' ' . $lastname, 'node/' . $node->nid); ?>
     </h4>
 
   <?php if (isset($content['field_biography_email'])): ?>
-    <div class="team-member__email field">
+    <div class="listing--cabinet__email field">
       <span class="label label--aligned">
         <?php print render($content['field_biography_email']['#title']); ?>
       </span>
@@ -48,7 +46,7 @@
   <?php endif; ?>
 
   <?php if (isset($phone)): ?>
-    <div class="team-member__phone field">
+    <div class="listing--cabinet__phone field">
       <span class="label label--aligned">
         <?php print render($content['field_biography_phone']['#title']); ?>
       </span>
@@ -57,11 +55,11 @@
   <?php endif; ?>
 
   <?php if (isset($social)): ?>
-    <div class="team-member__social field">
+    <div class="listing--cabinet__social field">
       <span class="label label--aligned--top">
         <?php print render($content['field_social_networks']['#title']); ?>
       </span>
-      <div class="team-member__social_icons">
+      <div class="listing--cabinet__social_icons">
       <?php
         foreach ($social as $i) {
           print render($content['field_social_networks'][$i]['#markup']);
@@ -72,11 +70,12 @@
   <?php endif; ?>
 
   </div> <!-- .team-member-top-wrapper END -->
-  <div class="field team-member-bottom-wrapper">
+
+  <div class="field listing--cabinet-bottom-wrapper">
     <span class="label label--aligned--top">
       <?php print render($content['body']['#title']); ?>
     </span>
-    <div class="team-member__body">
+    <div class="listing--cabinet__body">
       <?php print render($content['body'][0]['#markup']); ?>
     </div>
   </div> <!-- .team-member-bottom-wrapper END -->
