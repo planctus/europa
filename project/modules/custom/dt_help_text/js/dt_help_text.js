32538b884485e0818f3348a3ef7f04ae42667c66
Janos Kuszing <github.com@office.netstudio.hu>
2015-11-25 11:53:21 +0100

    NEXTEUROPA-7131: Finalize the solution.
    Former-commit-id: 13f5509492f801173df52f4f34a637fe44a9cb46

--- project/modules/custom/dt_help_text/js/dt_help_text.js
+++ project/modules/custom/dt_help_text/js/dt_help_text.js
@@ -1,11 +1,19 @@
-(function ($) {
-  Drupal.behaviors.dt_help_text = {
-    attach: function(context) {
-      $(".help-text-hidden").before('<a href="#">' + Drupal.t(' more ') + '</a>').hide();
-      $(".help-text-hidden").prev("a").bind('click', function($e){
+jQuery(function ($) {
+  // Find only the description fields which contain '[break]' text.
+  $('.description:contains("[break]")').each(function(index, value){
+    // [break] text replacement.
+    var replace_text = $(value).text().replace('[break]', '<a href="#">' + Drupal.t('more') + '</a><span class="help-text-hidden">') + '</span>';
+    // Replace the description field html with the new value.
+    $(value).html(replace_text);
+    // Find, hide, and bind a click listener on 'a' tag.
+    $(value)
+      .find('.help-text-hidden')
+      .hide()
+      .prev('a')
+      .bind('click', function(){
+        // Hide the 'a' tag, and show the hidden text.
         $(this).hide().next('.help-text-hidden').show();
         return false;
       });
-    }
-  }
-})(jQuery);
+  });
+});
