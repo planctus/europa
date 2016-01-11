076ca59d537d4d4cbd89c574e34cd2ba44a0a877
planctus <davide@nois3lab.it>
2016-01-11 17:59:10 +0100

    NEXTEUROPA-8024 - Political home page as a normal page
    Former-commit-id: 9288fbcc716cea1d839693822c9b9a102d95eed9
    Former-commit-id: 8ddee0a56b7f722373fb549a3f1cc4415067b0de

--- lib/themes/political/template.php
+++ lib/themes/political/template.php
@@ -5,13 +5,37 @@
  */
 
 /**
+ * Implements hook_preprocess_html().
+ */
+function political_preprocess_html(&$variables) {
+  // Remove the .front class from the body and pretend it's not the home page.
+  if ($variables['is_front']) {
+    $key = array_search('front', $variables['classes_array']);
+    unset($variables['classes_array'][$key]);
+  }
+}
+
+
+/**
  * Implements hook_preprocess_page().
  */
 function political_preprocess_page(&$variables) {
+  // Unset the home page.
+  $variables['is_front'] = FALSE;
+
   // Prepare the url for the "external" homepage.
   global $language;
   $delimiter = variable_get('language_suffix_delimiter', '_');
   $suffix = $delimiter . $language->prefix;
   // Set a variable containing the external url to point to.
   $variables['front_page'] = 'http://ec.europa.eu/index' . $suffix . '.htm';
+}
+
+/**
+ * Implementation of hook_context_load_alter().
+ */
+function political_context_load_alter(&$context) {
+  if ($context->name == 'dt_core_non_homepage_pages') {
+   unset($context->conditions['path']);
+  }
 }
