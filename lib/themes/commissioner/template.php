c9224f281d410d9e250385be66fd86dc2b6bf211
planctus <davide@nois3lab.it>
2016-01-14 16:37:10 +0100

    setting the external home page in the cwt
    Former-commit-id: 47ae91490baa65fa4865706d7c231cbeb730b80c
    Former-commit-id: 7213b519f3f7f03bddbdd0055b77111a735ba509

--- lib/themes/commissioner/template.php
+++ lib/themes/commissioner/template.php
@@ -6,6 +6,21 @@
  */
 
 /**
+ * Implements tempalate_preprocess_page().
+ */
+function commissioner_preprocess_page(&$variables) {
+  // Unset the home page.
+  $variables['is_front'] = FALSE;
+
+  // Prepare the url for the "external" homepage.
+  global $language;
+  $delimiter = variable_get('language_suffix_delimiter', '_');
+  $suffix = $delimiter . $language->prefix;
+  // Set a variable containing the external url to point to.
+  $variables['front_page'] = 'http://ec.europa.eu/index' . $suffix . '.htm';
+}
+
+/**
  * Returns an array with singular and plural form of a bundle.
  *
  * @param string $bundle
