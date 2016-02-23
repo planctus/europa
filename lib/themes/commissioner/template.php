67c1cd323ba3830c06bd2e3f5fbe12ced8b42463
Gingao Paulo <paulo.gingao@ext.ec.europa.eu>
2016-02-23 15:17:57 +0100

    Fixing PHPCS issues
    Former-commit-id: 9f6ca621adaf796db512b085fc6ac7320ae35a7d
    Former-commit-id: 146641134e5c806a8309dcd40d0afdefbda1d5fb

--- lib/themes/commissioner/template.php
+++ lib/themes/commissioner/template.php
@@ -17,7 +17,6 @@ function commissioner_preprocess_page(&$variables) {
   // Set a variable containing the external url to point to.
   $variables['front_page'] = 'http://ec.europa.eu/index' . $suffix . '.htm';
 
-
   // Adding string 'Tag: ' to Taxonomy tag pages.
   // Check if it's a taxonomy term page and object is loaded in $term.
   if (menu_get_object('taxonomy_term', 2)) {
