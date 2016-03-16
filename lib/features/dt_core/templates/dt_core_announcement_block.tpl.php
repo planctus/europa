a243801b1f8c73c45addf241cee86b85fc02e4f3
Harings Rob <haringsrob@gmail.com>
2016-03-16 15:58:07 +0100

    NEXTEUROPA-9585 full template and function
    Former-commit-id: a1e73f2c9b28d86d232859d5615af7af8b83daa5
    Former-commit-id: 99f40761d23c96d0af99e584205fea50cf600cbb

--- lib/features/dt_core/templates/dt_core_announcement_block.tpl.php
+++ lib/features/dt_core/templates/dt_core_announcement_block.tpl.php
@@ -1,5 +1,21 @@
-bar!
-
-<?php if ($heading): ?>
-  <h2 id="<?php echo drupal_clean_css_identifier($heading); ?>"><?php echo $heading; ?></h2>
-<?php endif; ?>
+<?php
+/**
+ * @file
+ * Contains the basic markup for the latest block.
+ */
+?>
+<div class="section__group">
+  <?php if ($heading): ?>
+    <h2 id="<?php echo drupal_clean_css_identifier($heading); ?>"><?php echo $heading; ?></h2>
+  <?php endif; ?>
+  <?php if ($news_items): ?>
+    <div class="sidebar-field-group__main">
+      <?php echo $news_items; ?>
+    </div>
+  <?php endif; ?>
+  <?php if ($social_links): ?>
+    <div class="sidebar-field-group__sidebar">
+      <?php echo $social_links; ?>
+    </div>
+  <?php endif; ?>
+</div>
