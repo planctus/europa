3659862a5bf9ada3aa5d743614ce9c62a8dec4c3
Nelson Matias <grin.correio@gmail.com>
2016-06-16 04:14:31 +0200

    DTTSB-537: Fixed phpcs errors.

--- lib/features/dt_core/templates/status_message.tpl.php
+++ lib/features/dt_core/templates/status_message.tpl.php
@@ -6,7 +6,7 @@
 ?>
 <?php if (!empty($message_title)) : ?>
   <div class="messages<?php print $message_classes . (!empty($message_type) ? ' ' . $message_type : ''); ?>">
-    <h3><?php if (!empty($message_type)) : ?><span class="sr-only"><?php print $message_type; ?> message: </span><?php endif; ?><?php print $message_title; ?></h3>
+    <h3><?php print (!empty($message_type) ? '<span class="sr-only">' . $message_type . ' message: </span>' : '') . $message_title; ?></h3>
     <?php if (!empty($message_body)) : ?>
       <p><?php print $message_body; ?></p>
     <?php endif; ?>
