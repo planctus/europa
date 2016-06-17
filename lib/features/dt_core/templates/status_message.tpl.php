d1f75624c5c32057ab657dbbf8dd3cfcc1da77b0
Nelson Matias <grin.correio@gmail.com>
2016-06-17 17:42:57 +0200

    DTTSB-537: Removed empty function from status_message template.

--- lib/features/dt_core/templates/status_message.tpl.php
+++ lib/features/dt_core/templates/status_message.tpl.php
@@ -4,10 +4,10 @@
  * Contains the markup for the message block.
  */
 ?>
-<?php if (!empty($message_title)) : ?>
-  <div class="messages<?php print $message_classes . (!empty($message_type) ? ' ' . $message_type : ''); ?>">
-    <h3><?php print (!empty($message_type) ? '<span class="sr-only">' . $message_type . ' message: </span>' : '') . $message_title; ?></h3>
-    <?php if (!empty($message_body)) : ?>
+<?php if ($message_title): ?>
+  <div class="messages<?php print $message_classes . ($message_type ? ' ' . $message_type : ''); ?>">
+    <h3><?php print ($message_type ? '<span class="sr-only">' . $message_type . ' message: </span>' : '') . $message_title; ?></h3>
+    <?php if ($message_body): ?>
       <?php print $message_body; ?>
     <?php endif; ?>
   </div>
