ab81af4e2a7d9f3af39a1f763609b8d786d73dd1
Nelson Matias <grin.correio@gmail.com>
2016-06-17 10:52:53 +0200

    DTTSB-537: Removed p tag from status_message template file.

--- lib/features/dt_core/templates/status_message.tpl.php
+++ lib/features/dt_core/templates/status_message.tpl.php
@@ -8,7 +8,7 @@
   <div class="messages<?php print $message_classes . (!empty($message_type) ? ' ' . $message_type : ''); ?>">
     <h3><?php print (!empty($message_type) ? '<span class="sr-only">' . $message_type . ' message: </span>' : '') . $message_title; ?></h3>
     <?php if (!empty($message_body)) : ?>
-      <p><?php print $message_body; ?></p>
+      <?php print $message_body; ?>
     <?php endif; ?>
   </div>
 <?php endif; ?>
