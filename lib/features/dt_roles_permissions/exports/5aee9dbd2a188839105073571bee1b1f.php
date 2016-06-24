fd5a4fee7784e88cd104fe05c6443962bad36454
Kalin Chernev <kalin.chernev@gmail.com>
2016-06-24 11:31:26 +0200

    Merging latest changes

--- lib/features/dt_roles_permissions/exports/5aee9dbd2a188839105073571bee1b1f.php
+++ lib/features/dt_roles_permissions/exports/5aee9dbd2a188839105073571bee1b1f.php
@@ -5,6 +5,8 @@
  */
 
 $permissions['editor'] = array(
+  'dt editorial mark linguistic review',
+  'dt editorial mark content review',
   'access administration menu',
   'access administration pages',
   'access broken links report',
