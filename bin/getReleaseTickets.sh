da14e5c37832389bc6da20f1e0bc49c7685d6703
Harings Rob <haringsrob@gmail.com>
2016-11-28 13:11:08 +0100

    Merge update hooks

--- bin/getReleaseTickets.sh
+++ bin/getReleaseTickets.sh
@@ -3,10 +3,15 @@
 # ./getReleaseTickets.sh master release/1
 # Then it will show all the links.
 
+echo Please make sure your local versions are up to date.
+
 SOURCE=$1
 TARGET=$2
 
+echo " "
+echo "#########################################"
 echo Issues in $TARGET going into $SOURCE:
+echo "#########################################"
 
 LOGS=$(git log $SOURCE..$TARGET --oneline)
 LIST=$(echo $LOGS | grep -Eo "DTTSB-[0-9]{1,5}" | sort -u)
@@ -14,4 +19,17 @@ LIST=$(echo $LOGS | grep -Eo "DTTSB-[0-9]{1,5}" | sort -u)
 while read -r line; do
   echo "https://webgate.ec.europa.eu/CITnet/jira/browse/$line"
 done <<< "$LIST"
+
+# Check the features that changed
+MODIFIEDFILES=$(git diff master --ignore-blank-lines | grep -e "\-\-\- a/lib/features/")
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENTDIR="$(dirname "$DIR")"
+
+echo " "
+echo "#########################################"
+echo "The following features have been updated:"
+echo "#########################################"
+
+php -f "$PARENTDIR/resources/scripts/other/ReleasePreparation.php" -- "UpdatedFeatures" "$MODIFIEDFILES"
 
