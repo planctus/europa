529d12cd6b8a426e9d877a0475f31993df0ce977
Harings Rob <haringsrob@gmail.com>
2016-12-12 13:08:50 +0100

    Small improvements to script

--- resources/scripts/other/ReleasePreparation.php
+++ resources/scripts/other/ReleasePreparation.php
@@ -17,12 +17,20 @@ switch ($argv[1]) {
       $matches = [];
       preg_match_all('/\/lib\/(?:modules|features|themes)\/([^\/]*)\/.*/', $argv[2], $matches);
       foreach (array_unique($matches[1]) as $updated) {
+        echo $updated . PHP_EOL;
+      }
+      foreach (array_unique($matches[1]) as $updated) {
+
+        $pull_request = 'https://github.com/ec-europa/dt_info_reference/pull/' . urlencode($argv[4]);
+        $checklisturl = 'https://webgate.ec.europa.eu/fpfis/wikis/display/MULTISITE/' . urlencode('[qa] ' . $updated . ' - release ' . $argv[3]);
+        $projecturl = 'https://webgate.ec.europa.eu/fpfis/wikis/display/MULTISITE/' . urlencode('[COMM] DT Information site');
+
         echo '-------- checklist title:' . PHP_EOL;
         echo '[qa] ' . $updated . ' - release ' . $argv[3] . PHP_EOL;
         echo '-------- Ticket body' . PHP_EOL;
-        echo 'Checklist: https://webgate.ec.europa.eu/fpfis/wikis/display/MULTISITE/[qa] ' . $updated . ' - release ' . $argv[3] . PHP_EOL;
-        echo 'PR: https://github.com/ec-europa/dt_info_reference/pull/' . $argv[3] . PHP_EOL;
-        echo 'Project: https://webgate.ec.europa.eu/fpfis/wikis/display/MULTISITE/[COMM] DT Information site' . PHP_EOL;
+        echo 'Checklist:' . $checklisturl . PHP_EOL;
+        echo 'PR:' . $pull_request . PHP_EOL;
+        echo 'Project:' . $projecturl . PHP_EOL;
       }
     }
     break;
